ea37bf3003ad11b871f884f75f73ab9a2c70c1d6
Amiel Paolo C. Liboro <amiel_liboro@yahoo.com.ph>
2019-03-11 19:52:23 +0800

    Uncontrolled Form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -23,7 +23,6 @@ class Contact extends Component {
         };
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleBlur = this.handleBlur.bind(this);
     }
 
     validate(firstname, lastname, telnum, email) {
