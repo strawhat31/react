b6add9572962ee945068d4cf896725417bf17163
Amiel Paolo C. Liboro <amiel_liboro@yahoo.com.ph>
2019-03-13 19:36:25 +0800

    React Redux Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -15,13 +15,13 @@ class Contact extends Component {
         alert('Current State is: ' + JSON.stringify(values));
     }
 
-    const required = (val) => val && val.length;
-    const maxLength = (len) => (val) => !(val) || (val.length <= len);
-    const minLength = (len) => (val) => val && (val.length >= len);
-    const isNumber = (val) => !isNaN(Number(val));
-    const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
-
     render(){
+
+        const required = (val) => val && val.length;
+        const maxLength = (len) => (val) => !(val) || (val.length <= len);
+        const minLength = (len) => (val) => val && (val.length >= len);
+        const isNumber = (val) => !isNaN(Number(val));
+        const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
         return(
             <div className="container">
