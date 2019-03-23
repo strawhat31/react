f9073e5435eca62bfdd90348a212e6c4db7d1d76
Amiel Paolo C. Liboro <amiel_liboro@yahoo.com.ph>
2019-03-23 18:37:58 +0800

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -21,8 +21,7 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -108,7 +107,7 @@ function RenderDish({dish}){
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
 
     const commentslist = comments.map((comment) => {
         return (
@@ -123,7 +122,7 @@ function RenderComments({comments}) {
             <h4>Comments</h4>
             <ul className="list-unstyled">
                 {commentslist}
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment}/>
             </ul> 
         </div>
     );
@@ -148,7 +147,10 @@ const DishDetail = (props) => {
                         <RenderDish dish={props.dish}/>
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments}/>
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}
+                        />
                     </div>
                 </div>
             </div>
